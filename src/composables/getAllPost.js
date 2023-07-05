f2985182b771090e938687c8b4618ad10d0a560d
Ken Ballon <bkenn003@gmail.com>
2023-07-05 16:30:19 +0800

    update js

--- src/composables/getAllPost.js
+++ src/composables/getAllPost.js
@@ -5,19 +5,21 @@ const getAllPost = () => {
 
   const getData = async () => {
     try {
-      let response = await fetch("http://localhost:3000/posts");
+      let response = await fetch("http://localhost:3000/blogPosts");
 
       if (!response.ok) {
         throw new Error("Unable to fetch data from the database...");
       }
       posts.value = await response.json();
-    } catch (error) {
-      error.value = error.message;
+
+      console.log(posts);
+    } catch (err) {
+      error.value = err.message;
       console.log(error.value);
     }
   };
 
-  return { posts };
+  return { posts, error, getData };
 };
 
 export default getAllPost;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,9 @@
 <script setup>
+import getAllPosts from '../composables/getAllPost.js'
+
+const { posts, error, getData } = await getAllPosts();
+
+console.log(posts);
 
 </script>
 
